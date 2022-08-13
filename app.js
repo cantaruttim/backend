434f82a7b62db03b326bdd514fac58e744704371
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-08-12 23:37:37 -0300

    conection to mysql

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const db = require('./models/db');
+
+const Home = require('./models/Home');
+
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
@@ -21,6 +27,20 @@ app.get('/', async(req, res) => {
             btn_link: "http://localhost:3000/contato"
         }
     })
+});
+
+app.post('/add-home', async(req, res) => {
+
+    await Home.create(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: "Dados da página home cadastrados com sucesso"
+    }).catch(() => {
+        return res.json({
+            erro: true,
+            mensagem: "Erro: Dados da página home não cadastrados com sucesso"
+    });
 });
 
 app.listen(8080, () => {
